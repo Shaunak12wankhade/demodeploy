2f3400fe45dfc2c304fe6fbe8b5ec5817c5ccb5f
unknown <shaunak12wankhade@ggmail.com>
2022-02-07 14:27:57 +0530

    [Shaunak] Added: demo deploy project

---
+++ demo-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Demo1Component } from './component/demo1/demo1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Demo1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
